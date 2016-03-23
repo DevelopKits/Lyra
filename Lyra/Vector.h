8b1ae7c952617af3891a4d4cfa4d5eddb2765042
Mirage <mirageyangkang@outlook.com>
2016-03-23 14:32:45 +0800

    Vector头文件的功能添加

--- Lyra/Vector.h
+++ Lyra/Vector.h
@@ -23,7 +23,54 @@ public:
 #endif
 
 	LVector();
-	explicit LVector(float InF);
-	LVector(float InX, float InY, float InZ);
+	explicit LVector( float InF );
+	LVector( float InX, float InY, float InZ );
 
+	LVector& operator=( const LVector& Other );
+	LVector& operator^( const LVector& V ) const;	
+	LVector& operator|( const LVector& V ) const;	
+	LVector operator+( const LVector& V ) const;
+	LVector operator+( float Bias ) const;
+	LVector operator-( const LVector& V ) const;
+	LVector operator-( float Bias ) const;
+	LVector operator*( float Scale ) const;
+	LVector operator*( const LVector& V ) const;
+	LVector operator/( float Scale ) const;
+	LVector operator/( const LVector& V ) const;
+	LVector operator==( const LVector& V ) const;
+	LVector operator!=( const LVector& V ) const;
+	LVector operator-() const;
+	LVector operator+=( const LVector& V );
+	LVector operator-=( const LVector& V );
+	LVector operator*=( float Scale );
+	LVector operator/=( float Scale );
+	LVector operator*=( const LVector& V );
+	LVector operator/=( const LVector& V );
+	float& operator[]( int Index );
+	float operator[]( int Index ) const;
+
+	float& Component( int Index );
+	float Component( int Index ) const;
+	bool Equals( const LVector& V, float Tolerance ) const;
+	bool AllComponentsEqual( float Tolerance ) const;
+	void Set( float InX, float InY, float InZ );
+	float GetMax() const;
+	float GetAbsMax() const;
+	float GetMin() const;
+	float GetAbsMin() const;
+	LVector ComponentMin( const LVector& Other ) const;
+	LVector ComponentMax( const LVector& Other ) const;
+	LVector GetAbs() const;
+	float Size() const;
+	float SizeSquared() const;
+	float Size2D() const;
+	float SizeSquared2D() const;
+	bool IsNearlyZero( float Tolerance ) const;
+	bool IsZero() const;
+	bool Normalize( float Tolerance );
+	bool IsNormalized() const;
+	void ToDirectionAndLength( LVector &OutDir, LVector &Length ) const;
+	LVector GetSignVector() const;
+	static LVector CrossProduct( const LVector& A, const LVector& B );
+	static LVector DotProduct( const LVector& A, const LVector& B );
 };
